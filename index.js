86fded6145ec8723e9c1ee025219c9edbda68122
Visnu Pitiyanuvath <visnup@gmail.com>
2020-09-22 10:18:08 -0700

    Just need jsdom + fetch

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-import puppeteer from "puppeteer";
+import fetch from "node-fetch";
+import jsdom from "jsdom";
+import {URL} from "url";
 
+const base =
+  "https://www.onlyinboards.com/Used-ski-boats.aspx?MakerName=1&YearFrom=2018&YearTo=&PriceFrom=&PriceTo=&Model=221&ZIP=&Distance=";
 (async function () {
-  const browser = await puppeteer.launch({
-    defaultViewport: {width: 1280, height: 900},
-  });
-  const page = await browser.newPage();
-  await page.setUserAgent(
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
-  );
-  await page.goto(
-    "https://www.onlyinboards.com/Used-ski-boats.aspx?MakerName=1&YearFrom=2018&YearTo=&PriceFrom=&PriceTo=&Model=221&ZIP=&Distance="
-  );
-  const links = await page.$(".info a");
-  console.log(links);
-  await page.screenshot({path: `results.png`});
-  browser.close();
+  const res = await fetch(base);
+  const {
+    window: {document},
+  } = new jsdom.JSDOM(await res.text());
+
+  const boats = [];
+  for (const link of document.querySelectorAll(".info a")) {
+    console.log(link.href);
+    boats.push(await boat(link.href));
+  }
+
+  console.log(boats);
 })();
+
+async function boat(path) {
+  const url = new URL(path, base);
+  const res = await fetch(url);
+  const html = await res.text();
+  const {
+    window: {document},
+  } = new jsdom.JSDOM(html);
+
+  const selectors = {
+    name: "[itemprop='name']",
+    price: ".price strong span",
+    listed: ".datelisted strong",
+    engine: ".boat-info li:first-child strong",
+    hours: ".col-right .boat-info li.hours strong",
+    status: ".hours strong font",
+    location: ".location strong",
+    description: "[itemprop='description'] p",
+    year: ".year strong",
+    make: ".make strong",
+    model: ".model strong",
+  };
+
+  return {
+    ...Object.fromEntries(
+      Object.entries(selectors).map(([key, selector]) => [
+        key,
+        (document.querySelector(selector) || {innerHTML: ""}).innerHTML
+          .trim()
+          .replace(/\s+/g, " "),
+      ])
+    ),
+    url: url.toString(),
+  };
+}
